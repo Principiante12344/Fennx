a6815e2bf54bd2f45a55ae72572be1667cf1b525
Kennn <111826360+Principiante12344@users.noreply.github.com>
2024-04-21 12:28:05 -0500

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,40 +1,16 @@
-import fetch from 'node-fetch';
+import Scraper from '@SumiFX/Scraper'
 
-const handler = async (m, { conn, args }) => {
-    
-    if (!args[0]) {
-        throw `Por favor, ingresa un enlace de Instagram para descargar el archivo.`;
-    }
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply('🍭 Ingresa el enlace del vídeo de Instagram junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.instagram.com/reel/CijhxhAD53X/?igsh=amJqMDQ1cW9zOG9s`)
 
-    try {
-        const apiUrl = `https://aemt.me/download/igdl?url=${encodeURIComponent(args[0])}`;
-        const response = await fetch(apiUrl);
-        const responseData = await response.json();
-
-        m.react(rwait);
-
-        if (responseData.status && responseData.result.length > 0) {
-            for (const media of responseData.result) {
-                m.react(done);
-                await conn.sendFile(m.chat, media.url, media.url.includes('.mp4') ? 'video.mp4' : 'imagen.jpg', '', m);
-            }
-        } else {
-            throw `
-> Sin respuesta
-
-No se pudo obtener el contenido de Instagram.`;
-        }
-    } catch (error) {
-        console.error(error);
-        throw `
-> Sin respuesta
-
-Ocurrió un error al procesar la solicitud: ${error.message}`;
-    }
-};
-
-handler.help = ['instagram'];
-handler.tags = ['dl'];
-handler.command = /^(instagramdl|instagram|igdl|ig)$/i;
-
-export default handler;
+try {
+let { dl_url } = await Scraper.igdl(args[0])
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: null }, { quoted: m})
+} catch {
+}}
+handler.help = ['instagram <url ig>']
+handler.tags = ['downloader']
+handler.command = ['ig', 'igdl', 'instagram']
+handler.register = true 
+//handler.limit = 1
+export default handler
