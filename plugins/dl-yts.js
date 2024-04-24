d0b332dc719838f9110eb0f8c2be07f661b886a8
Kennn <111826360+Principiante12344@users.noreply.github.com>
2024-04-23 22:03:12 -0500

    Update dl-yts.js

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -1,10 +1,16 @@
-import Scraper from "@SumiFX/Scraper"
+import Scraper from "@SumiFX/Scraper" // Importa el módulo Scraper desde "@SumiFX/Scraper"
 
+// Define un manejador de eventos asincrónico para el comando 'ytsearch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
+    // Verifica si se proporcionó un argumento (título de un video o canción de YouTube)
     if (!text) return conn.reply(m.chat, '🍭 Ingresa el título de un video o canción de YouTube.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Gemini Aaliyah - If Only`, m)
+    // Realiza una búsqueda en YouTube del texto proporcionado y obtiene los resultados
     let results = await Scraper.ytsearch(text)
+    // Verifica si se encontraron resultados
     if (!results || !results.length) return conn.reply(m.chat, `No se encontraron resultados.`, m)
+    // Obtiene la URL de la miniatura del primer resultado
     let img = results[0].thumbnail
+    // Construye un mensaje con los detalles de cada resultado
     let txt = `╭─⬣「 *YouTube Search* 」⬣\n`
     results.forEach((video, index) => {
         txt += ` │  ≡◦ *🐢 Nro ∙* ${index + 1}\n`
@@ -16,10 +22,13 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
         txt += ` ╰──────────⬣`
         txt += `\n`
     })
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
+    // Envía la miniatura y el mensaje con los detalles de los resultados al chat
+    await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m)
 }
-handler.help = ['ytsearch <búsqueda>']
-handler.tags = ['search']
-handler.command = ['ytsearch', 'yts']
-handler.register = true 
-export default handler
+
+handler.help = ['ytsearch <búsqueda>'] // Define la ayuda para el comando 'ytsearch'
+handler.tags = ['search'] // Etiqueta el comando como de búsqueda
+handler.command = ['ytsearch', 'yts'] // Define los comandos relacionados con 'ytsearch'
+handler.register = true // Indica que el manejador debe registrarse en el bot
+
+export default handler // Exporta el manejador como predeterminado
