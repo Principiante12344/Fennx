bac74d398d15652cda2dccd6f8563a2a31824fdd
Kennn <111826360+Principiante12344@users.noreply.github.com>
2024-04-27 15:13:21 -0500

    Update dl-mediafire.js

--- plugins/dl-mediafire.js
+++ plugins/dl-mediafire.js
@@ -1,63 +1,24 @@
+import Scraper from "@SumiFX/Scraper"
 
-import fetch from 'node-fetch'
-import { mediafiredl } from '@bochilteam/scraper'
-import fg from 'api-dylux'
-let free = 150 // limite de descarga
-let prem = 500
-let handler = async (m, { conn, args, text, usedPrefix, command, isOwner, isPrems }) => {
-	  
-   if (!args[0]) throw `✳️ ${mssg.noLink('Mediafire')}`
-    if (!args[0].match(/mediafire/gi)) throw `❎ ${mssg.noLink('Mediafire')}`
-    m.react(rwait)
-
-    let limit = isPrems || isOwner ? prem : free
-	  let u = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
-    let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
-    try {
-    let res = await mediafiredl(args[0])
-    let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let isLimit = limit * 1024 < filesize
-    let caption = `
-   ≡ *MEDIAFIRE DL*
-
-*📌${mssg.name}:* ${filename}
-*⚖️${mssg.size}:* ${filesizeH}
-*🔼${mssg.aploud}:* ${aploud}
-${isLimit ? `\n▢ ${mssg.limitdl} *+${free} MB* ${mssg.limitdlTe} *${prem} MB*` : ''} 
-`.trim()
-    await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
-    if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-
-  } catch { 
-  
-  try {
-	let res = await fg.mediafireDl(args[0])
-    let { url, url2, filename, ext, upload_date, filesize, filesizeB } = res
-   
-	   let isLimit = limit * 1024 < filesizeB
-    let caption = `
-   ≡ *MEDIAFIRE DL 2*
-
-*📌${mssg.name}:* ${filename}
-*⚖️${mssg.size}:* ${filesize}
-*🔼${mssg.aploud}:* ${upload_date}
-${isLimit ? `\n▢ ${mssg.limitdl} *+${free} MB* ${mssg.limitdlTe} *${prem} MB*` : ''} 
-`.trim()
-await conn.sendFile(m.chat, ss, 'ssweb.png', caption, m)
-if(!isLimit) await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
-    m.react(done)
-  } catch {
-    m.reply(mssg.error)
-  }
-
-  }
-
-}
-handler.help = ['mediafire <url>']
-handler.tags = ['dl', 'prem']
-handler.command = ['mediafire', 'mfire'] 
-handler.diamond = true
-handler.premium = false
-
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply('🍭 Ingresa el enlace del archivo de Mediafire junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.mediafire.com/file/433hbpsc95unywu/Oshi_no_Ko_01.mp4/file?dkey=jpivv6z5osa&r=1587`)
+if (!args[0].match(/mediafire/gi)) return m.reply('El enlace deve ser de un archivo de Mediafire.')
+try {
+let { title, ext, aploud, size, dl_url } = await Scraper.mediafire(args[0])
+if (size.includes('GB') || size.replace(' MB', '') > 300) { return await m.reply('El archivo pesa mas de 300 MB, se canceló la Descarga.')}
+let txt = `╭─⬣「 *Mediafire Download* 」⬣\n`
+    txt += `│  ≡◦ *🍭 Nombre ∙* ${title}\n`
+    txt += `│  ≡◦ *🪴 Subido ∙* ${aploud}\n`
+    txt += `│  ≡◦ *📚 MimeType ∙* ${ext}\n`
+    txt += `│  ≡◦ *⚖ Peso ∙* ${size}\n`
+    txt += `╰─⬣`
+await m.reply(txt)
+await conn.sendFile(m.chat, dl_url, title, null, m, null, { mimetype: ext, asDocument: true })
+} catch {
+}}
+handler.help = ['mediafire <url mf>']
+handler.tags = ['downloader']
+handler.command = ['mediafire', 'mdfire', 'mf']
+handler.register = true 
+handler.limit = 500
 export default handler
